d6554b4fc19b0e5ce5556deb3b41db4c51f2fd69
prateeksahu147 <prateeksahu147@gmail.com>
2023-05-21 01:33:55 +0530

    add source code

---
+++ neural-roots-server/neural_roots_workspace/api/serializers/Employee/EmployeeSerializer.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from ....models.Employee.EmployeeDetails import EmployeeDetails
+
+class EmployeeListSerializer(ModelSerializer):
+    class Meta:
+        model = EmployeeDetails
+        fields = '__all__'
